35f285bbb323bb2ac2fb8ab384fb052af32bf72f
Stiv <strodriguezna@gmail.com>
2024-12-24 19:25:25 -0500

    first commit add project msvc-usuarios

---
+++ curso-kubernetes/msvc-usuarios/src/test/java/com/stiv/springcloud/mcsv/msvcusuarios/MsvcUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stiv.springcloud.mcsv.msvcusuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcUsuariosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ curso-kubernetes/src/main/java/com/stiv/springcloud/mcsv/Main.java
@@ -0,0 +1,7 @@
+package com.stiv.springcloud.mcsv;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
